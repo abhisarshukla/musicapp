2f720259b9c12665cb91cd726d8c3eb89c89a361
abhisarshukla <abhisarshukla645@gmail.com>
2019-10-23 23:39:22 +0530

    Added database classes and corrected template/base.html file

--- musicapp.py
+++ musicapp.py
@@ -8,10 +8,31 @@ app.config['SECRET_KEY'] = '68307df7029781e7602f3c4e2522b875'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
-'''
-TODO:
-Add relationships to other classes.
-'''
+listened = db.Table('listened',
+            db.Column('user_id' ,db.Integer, db.ForeignKey('user.id'))
+            db.Column('sond_id', db.Integer, db.ForeignKey('song.id'))
+)
+
+includes = db.Table('includes',
+            db.Column('playlist_id', db.Integer, db.ForeignKey('playlist.id')),
+            db.Column('song_id', db.Integer, db.ForeignKey('song.id'))
+)
+
+composed = db.Table('composed',
+            db.Column('artist_id', db.Integer, db.ForeignKey('artist.id')),
+            db.Column('song_id', db.Integer, db.ForeignKey('song.id'))
+)
+
+subscribed = db.Table('subscribed',
+            db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+            db.Column('podcast_id', db.Integer, db.ForeignKey('podcast.id'))
+)
+
+contains = db.Table('contains',
+            db.Column('song_id', db.Integer, db.ForeignKey('song.id')),
+            db.Column('playlist_id', db.Integer, db.ForeignKey('playlist.id'))
+)
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     fname = db.Column(db.String(75), nullable=False)
@@ -22,10 +43,13 @@ class User(db.Model):
     image_file = db.Column(db.String(20), nullable=False, default='default.jpeg')
     preflang = db.Column(db.String(20), nullable=False)
     password = db.Column(db.String(60), nullable=False)
+    searches = db.relationshp('user_search_history', backref=db.backref('user'))
+    playlists = db.relationship('user_song_playlist', backref=db.backref('user'))
+    songs = db.relationship('Song', secondary=listened)
+    podcasts = db.relationship('Podcast', secondary=subscribed)
 
     def __repr__(self):
-        return f"User('{self.username}', '{self.fname}', '{self.lname}',
-                '{self.email}', '{self.image_file}')"
+        return f"User('{self.username}', '{self.fname}', '{self.lname}', '{self.email}', '{self.image_file}')"
 
 '''
 TODO:
@@ -37,9 +61,11 @@ class Podcast(db.Model):
     name = db.Column(db.String, nullable=True)
     category = db.Column(db.String, nullable=False)
     release_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    file_location = db.Column(db.String(20), nullale=True)
     rating = db.Column(db.Integer, default=2)
     likes = db.Column(db.Integer)
     dislikes = db.Column(db.Integer)
+    artists = db.relationship('Artist', backref=db.backref('podcast'))
 
     def __repr__(self):
         return f"Podcast('{self.name}', '{self.category}', '{self.release_date}''{self.rating}')"
@@ -47,31 +73,39 @@ class Podcast(db.Model):
 '''
 TODO:
 Add relationships to other classes.
-'''class songs(db.Model):
+'''
+class Song(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    songname = db.Column(db.String(75), nullable=False)
+    name = db.Column(db.String(75), nullable=False)
     duration = db.Column(db.Float, nullable=False)
     release_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     rating = db.Column(db.Integer, default=2)
     likes = db.Column(db.Integer)
     dislikes = db.Column(db.Integer)
+    file_location = db.Column(db.String(20), nullable=False)
     album = db.Column(db.String(75), nullable=False)
     genre = db.Column(db.String(75), nullable=False)
 
-class admin(db.Model):
+    def __repr__(self):
+        return f"Song('{self.name}', '{self.release_date}', '{self.rating}')"
+
+class Admin(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     fname = db.Column(db.String(75), nullable=False)
     fname = db.Column(db.String(75), nullable=False)
     lname = db.Column(db.String(75), nullable=False)
 
+    def __repr__(self):
+        return f"Admin('{self.fname}', '{self.lname}')"
+
 '''
 TODO:
 Add relationship to other tables.
 '''
-class artist(db.Model):
+class Artist(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     fname = db.Column(db.String(75), nullable=False)
-    fname = db.Column(db.String(75), nullable=False)
+    mname = db.Column(db.String(75), nullable=False)
     lname = db.Column(db.String(75), nullable=False)
     email = db.Column(db.String(128), unique=True, nullable=False)
     image_file = db.Column(db.String(20), nullable=False, default='default.jpeg')
@@ -80,54 +114,31 @@ class artist(db.Model):
     likes = db.Column(db.Integer)
     dislikes = db.Column(db.Integer)
     rating = db.Column(db.Integer, default=2)
+    podcast = db.Column(db.Integer, db.ForeignKey('podcast.id'))
+    compose = db.relationship('Song', secondary=composed, backref=db.backref('conposer', lazy='dynamic'))
 
-class playlist(db.Model):
+
+    def __repr(self):
+        return f"Artist('{self.fname}', '{self.lname}', '{self.rating}')"
+
+class Playlist(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     likes = db.Column(db.Integer)
     dislikes = db.Column(db.Integer)
     rating = db.Column(db.Integer, default=2)
+    user = db.Column(db.Integer, db.ForeignKey('user.id'))
+    songs = db.relationship('Song', secondary=contains)
 
-'''
-TODO:
-Add relationship to other tables.
-'''
-class contains(db.Model):
-    pass
-
-'''
-TODO:
-Add relationship to other tables.
-'''
-class composed(db.Model):
-    pass
-
-'''
-TODO:
-Add relationship to other tables.
-'''
-class includes(db.Model):
-    pass
-
-'''
-TODO:
-Add relationship to other tables.
-'''
-class listened(db.Model):
-    pass
-
-'''
-TODO:
-Add relationship to other tables.
-'''
-class subscribed(db.Model):
-    pass
+    def __repr__(self):
+        return f"Playlist('{self.id}', '{self.rating}')"
 
 '''
 TODO:
 Add relationship to other tables.
 '''
 class user_song_playlist(db.Model):
-    pass
+    user = db.Column(db.Integer, db.ForeignKey('user.id'))
+    songs = db.relationship('Song', secondary=includes)
 
 '''
 TODO:
@@ -137,6 +148,7 @@ class user_search_history(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     datetime = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     searchno = db.Column(db.Integer, nullable=False)
+    user = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 posts = [
     {
